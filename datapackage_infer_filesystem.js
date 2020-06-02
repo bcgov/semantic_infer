4d371a028b3408fc71d3944268f75636fe1684e7
Paul Ripley <Paul.Ripley@avocette.com>
2020-06-02 08:44:23 -0700

    initial semantic infer package commit

---
+++ datapackage_infer_filesystem.js
@@ -0,0 +1,52 @@
+const {Package} = require('datapackage');
+
+module.exports = {
+	infer_datapackage: function() {
+		return infer_datapackage();
+	}
+};
+
+const infer_datapackage = async () => {
+	
+	const dataPackage = await Package.load();
+	const semanticinfer = require('./semantic_infer');
+	const constants = require('./constants');
+	const VAR_CLASS_ATTR = constants.VAR_CLASS_ATTR;
+	const RDF_ATTR = constants.RDF_ATTR;
+	const SAVED_PATH_ATTR = constants.SAVED_PATH_ATTR;
+	const DATA_PACKAGE_INFER_FILE_FILTER = constants.DATA_PACKAGE_INFER_FILE_FILTER;
+	var vals = [];
+	var fieldVals = [];
+	var resourceDataSample = [];
+	var i;
+	var s;
+	var resource;
+	try {
+		await dataPackage.infer(DATA_PACKAGE_INFER_FILE_FILTER);
+		for (r in dataPackage.descriptor.resources) {
+			resource = await dataPackage.resources[r];
+			//check for semantic inference only if files are identified as being tabular
+			if (resource.tabular) {
+				resourceDataSample = await resource.read(keyed=true,limit=10);
+				for (f in resource.schema.fields){
+					field = resource.schema.fields[f];
+					fieldVals = [];
+					for (j = 0; j < resourceDataSample.length; j++){
+						fieldVals[j] = resourceDataSample[j][field.name];
+					}
+					s = semanticinfer.semantically_classify_field(field.name, fieldVals, field.type);
+					if (s != 'none') {
+						dataPackage.descriptor.resources[r].schema.fields[f][VAR_CLASS_ATTR] = s[VAR_CLASS_ATTR];
+						if (RDF_ATTR in s) {
+							dataPackage.descriptor.resources[r].schema.fields[f][RDF_ATTR] = s[RDF_ATTR];
+						}
+					}
+				}
+			}
+		}
+		dataPackage.commit();
+		console.log(JSON.stringify(dataPackage.descriptor, null, 4));
+	} catch(err){console.log('Error occurred: '); console.log(err.message); console.log(dataPackage.errors);}
+	
+} 
+infer_datapackage();
---
+++ index.js
@@ -0,0 +1,4 @@
+const semantic_infer = require('./semantic_infer');
+const datapackage_infer = require('./datapackage_infer');
+const datapackage_infer_filesystem = require('./datapackage_infer_filesystem');
+module.exports = { datapackage_infer, semantic_infer, datapackage_infer_filesystem };
